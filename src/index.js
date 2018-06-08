5cb2c2631bc4be66ed56684411813da81871eb01
David <dvdsosa@gmail.com>
2018-06-08 10:26:56 +0100

    clase 27 - refactor del index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 'use strict'
 
 // instanciando los objetos app y BrowserWindow
-import { app, BrowserWindow, ipcMain, dialog } from 'electron'
+import { app, BrowserWindow } from 'electron'
 import devtools from './devtools'
+import setIpcMain from './ipcMainEvents'
 import handleErrors from './handle-errors'
-import isImage from 'is-image'
-import filesize from 'filesize'
-import fs from 'fs'
-import path from 'path'
 
 if (process.env.NODE_ENV === 'development') {
   devtools()
@@ -33,6 +30,7 @@ app.on('ready', () => {
     show: false
   })
 
+  setIpcMain(win)
   handleErrors(win)
 
   win.once('ready-to-show', () => {
@@ -53,57 +51,4 @@ app.on('ready', () => {
   // win.loadURL('http://devdocs.io/')
   win.loadURL(`file://${__dirname}/renderer/index.html`)
   win.toggleDevTools()
-})
-
-ipcMain.on('open-directory', (event) => {
-  dialog.showOpenDialog(win, {
-    title: 'Selecciona la nueva ubicación',
-    buttonLabel: 'Abrir ubicación',
-    properties: ['openDirectory']
-  },
-  (dir) => {
-    const images = []
-    if (dir) {
-      fs.readdir(dir[0], (err, files) => {
-        if (err) throw err
-
-        for (let i = 0; i < files.length; i++) {
-          if (isImage(files[i])) {
-            let imageFile = path.join(dir[0], files[i])
-            let stats = fs.statSync(imageFile)
-            let size = filesize(stats.size, {round: 0})
-            images.push({
-              filename: files[i],
-              src: `file://${imageFile}`,
-              size: size
-            })
-          }
-        }
-
-        event.sender.send('load-images', images)
-      })
-    }
-  })
-})
-
-ipcMain.on('open-save-dialog', (event, ext) => {
-  dialog.showSaveDialog(win, {
-    title: 'Guardar imagen modificada',
-    buttonLabel: 'Guardar imagen',
-    filters: [{name: 'Images', extensions: [ext.substr(1)]}]
-  },
-  file => {
-    if (file) {
-      event.sender.send('save-image', file)
-    }
-    console.log(file)
-  })
-})
-
-ipcMain.on('show-dialog', (event, info) => {
-  dialog.showMessageBox(win, {
-    type: info.type,
-    title: info.title,
-    message: info.message
-  })
 })
